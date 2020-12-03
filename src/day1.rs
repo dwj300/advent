51bbb407813a72fe4a4179f0d91132a14b4bd516
Douglas Jordan <dojordan@microsoft.com>
2020-12-02 16:28:56 -0800

    cleaning up

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,22 @@
 use std::collections::HashMap;
 use std::fs;
 
-fn part2(input: Vec<i32>) -> i32 {
+fn part1(input: &Vec<i32>) -> i32 {
+    let mut partials = HashMap::<i32, i32>::new();
+    for i in input.iter() {
+        partials.insert(2020-i, *i);
+    }
+
+    for i in input.iter() {
+        match partials.get(i) {
+            Some(partial) => return partial * i,
+            _ => ()
+        }
+    }
+    return -1;
+}
+
+fn part2(input: &Vec<i32>) -> i32 {
     let mut partials = HashMap::<i32, i32>::new();
     for i in input.iter() {
         partials.insert(2020-i, *i);
@@ -21,43 +36,21 @@ fn part2(input: Vec<i32>) -> i32 {
     return -1;
 }
 
-fn part1(input: Vec<i32>) -> i32 {
-    let mut partials = HashMap::<i32, i32>::new();
-    for i in input.iter() {
-        partials.insert(2020-i, *i);
+fn runner(func: &dyn Fn(&Vec<i32>) -> i32, sample: &Vec<i32>, expected: i32, input: &Vec<i32>, answer: Option<i32>) {
+    assert_eq!(func(sample), expected);
+    let ans = func(input);
+    println!("{}", ans);
+    match answer {
+        Some(x) => {
+            assert_eq!(ans, x);
+        },
+        None => ()
     }
-
-    for i in input.iter() {
-        match partials.get(i) {
-            Some(partial) => return partial * i,
-            _ => ()
-        }
-    }
-    return -1;
 }
 
 pub fn day1() {
-    let sample = [1721,979,366,299,675,1456];
-    let ans = part1(sample.to_vec());
-    assert_eq!(ans, 514579);
-    let ans2 = part2(sample.to_vec());
-    assert_eq!(ans2, 241861950);
-
-    let contents = fs::read_to_string("day1.txt")
-        .expect("Something went wrong reading the file");
-
-    let input: Vec<&str> = contents.lines().collect();
-    let mut nums = Vec::new();
-    for line in input.iter() {
-        let i: i32 = line.parse().unwrap_or(-1);
-        nums.push(i)
-    }
-    let nums2 = nums.clone();
-    let ans3 = part1(nums);
-    println!("{}", ans3);
-    assert_eq!(ans3, 842016);
-
-    let ans4 = part2(nums2);
-    println!("{}", ans4);
-    assert_eq!(ans4, 9199664);
+    let sample = [1721,979,366,299,675,1456].to_vec();
+    let nums: Vec<i32> = fs::read_to_string("day1.txt").unwrap().lines().map(|line| line.parse::<i32>().unwrap()).collect();
+    runner(&part1, &sample, 514579, &nums, Some(842016));
+    runner(&part2, &sample, 241861950, &nums, Some(9199664));
 }
